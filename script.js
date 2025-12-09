b8097199d1c3b15788f18417df6c1e4523e9c9f3
CrazyBot <harshpaliwal0210@gmail.com>
2025-12-09 15:12:18 +0530

    Refactor script.js to remove blank lines
    Removed unnecessary blank lines for cleaner code.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 const dingSound = new Audio('https://assets.mixkit.co/active_storage/sfx/2578/2578-preview.mp3');
+
 const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
@@ -12,40 +12,30 @@ function addTask() {
         return;
     }
 
-   
     const li = document.createElement('li');
-    
     
     const span = document.createElement('span');
     span.innerText = text;
-    
     
     const deleteBtn = document.createElement('button');
     deleteBtn.innerText = 'X';
     deleteBtn.className = 'delete-btn';
 
-   
     li.appendChild(span);
     li.appendChild(deleteBtn);
     taskList.appendChild(li);
 
-   
     taskInput.value = '';
 }
 
-
 addBtn.addEventListener('click', addTask);
 
-
 taskList.addEventListener('click', function(e) {
-    
     
     if (e.target.classList.contains('delete-btn')) {
         e.target.parentElement.remove();
     } 
-    
     else {
-       
         const item = e.target.closest('li');
         
         if (item) {
